e8303b61d7687cd7208e8a0d3e0ca3e10da303ff
Antonio Guedes <agsoares@gmail.com>
2020-09-13 16:59:14 -0300

    adição de validação de cep

--- cadastrarpaciente.php
+++ cadastrarpaciente.php
@@ -156,15 +156,15 @@
 			                    url: 'https://viacep.com.br/ws/'+$(this).val()+'/json/unicode/',
 			                    dataType: 'json',
 			                    success: function(resposta){
-				                    $("#logradouro").val(resposta.logradouro);
-				                    $("#complemento").val(resposta.complemento);
-				                    $("#bairro").val(resposta.bairro);
-				                    $("#cidade").val(resposta.localidade);
-				                    $("#uf").val(resposta.uf);
-                                    $("#numero").focus();
-                                },
-                                error: function () {
-                                    alert("CEP não encontrado!!");
+                                    if (resposta == 1) {
+				                        $("#logradouro").val(resposta.logradouro);
+				                        $("#complemento").val(resposta.complemento);
+				                        $("#bairro").val(resposta.bairro);
+				                        $("#cidade").val(resposta.localidade);
+				                        $("#uf").val(resposta.uf);
+                                        $("#numero").focus();
+                                    }
+                                    else {alert("CEP não encontrado!!");}
                                 }
 		                    });
 	                    });
