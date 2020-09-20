11b388f91ccfadbf71577932cd9c9957b5404bce
Antonio Guedes <agsoares@gmail.com>
2020-09-19 23:50:04 -0300

    Inicio do ajuste do botoes

--- dashboard.php
+++ dashboard.php
@@ -19,13 +19,13 @@
 <div data-role="panel" data-title-caption="Agenda" data-collapsible="false" data-title-icon="<span class='mif-calendar'></span>" class="mt-4">
     <div class="row">
         <div class="cell-md-8 p-10">
-        <h5 class="text-center" >Event Calendar with jQuery, PHP and MySQL</h5>	
-        <div class="page-header">
+        <h5 class="text-center" >Dr(a). Juliana Nunes Calil</h5>	
+            <div class="page-header">
                 <div class="pull-right form-inline">
                     <div class="btn-group">
-                        <button class="btn btn-primary" data-calendar-nav="prev"><< Prev</button>
-                        <button class="btn btn-default" data-calendar-nav="today">Today</button>
-                        <button class="btn btn-primary" data-calendar-nav="next">Next >></button>
+                        <button class="button primary" data-calendar-nav="prev"><< Ant</button>
+                        <button class="button success" data-calendar-nav="today">Hoje</button>
+                        <button class="button primary" data-calendar-nav="next">Próx >></button>
                     </div>
                     <div class="btn-group">
                         <button class="btn btn-warning" data-calendar-view="year">Year</button>
@@ -34,8 +34,6 @@
                         <button class="btn btn-warning" data-calendar-view="day">Day</button>
                     </div>
                 </div>
-                <h3></h3>
-                <small>To see example with events navigate to Februray 2018</small>
             </div>
             <div class="row">
                 <div class="col-md-9">
