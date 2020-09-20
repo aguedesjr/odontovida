faf4b01ac9d92030432d41fc0aa1e2feabc856c9
Antonio Guedes <agsoares@gmail.com>
2020-09-19 23:13:24 -0300

    Tentativa de adição do calendário

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,7 @@
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="css/calendar.css">
+
 <div class="row border-bottom bd-lightGray m-3">
     <div class="cell-md-4 d-flex flex-align-center">
         <h3 class="dashboard-section-title  text-center text-left-md w-100">Pandora <small>Version 1.0</small></h3>
@@ -14,48 +18,45 @@
 <div class="m-3">
 <div data-role="panel" data-title-caption="Agenda" data-collapsible="true" data-title-icon="<span class='mif-calendar'></span>" class="mt-4">
     <div class="row">
-        <div class="cell-md-8 p-10">
-            <h5 class="text-center">Sales: 1 Jan, 2014 - 30 Jul, 2014</h5>
-            <canvas id="dashboardChart1"></canvas>
-        </div>
-        <div class="cell-md-4 p-10">
-            <h5 class="text-center">Goal Completion</h5>
-            <div class="mt-6">
-                <div class="clear">
-                    <div class="place-left">Add Products to Cart</div>
-                    <div class="place-right"><strong>160</strong>/200</div>
+            <div class="container">	
+            <h2>Event Calendar with jQuery, PHP and MySQL</h2>	
+            <div class="page-header">
+                <div class="pull-right form-inline">
+                    <div class="btn-group">
+                        <button class="btn btn-primary" data-calendar-nav="prev"><< Prev</button>
+                        <button class="btn btn-default" data-calendar-nav="today">Today</button>
+                        <button class="btn btn-primary" data-calendar-nav="next">Next >></button>
+                    </div>
+                    <div class="btn-group">
+                        <button class="btn btn-warning" data-calendar-view="year">Year</button>
+                        <button class="btn btn-warning active" data-calendar-view="month">Month</button>
+                        <button class="btn btn-warning" data-calendar-view="week">Week</button>
+                        <button class="btn btn-warning" data-calendar-view="day">Day</button>
+                    </div>
                 </div>
-                <div data-role="progress" data-value="35" data-cls-bar="bg-cyan"></div>
+                <h3></h3>
+                <small>To see example with events navigate to Februray 2018</small>
             </div>
-            <div class="mt-6">
-                <div class="clear">
-                    <div class="place-left">Complete Purchase</div>
-                    <div class="place-right"><strong>310</strong>/400</div>
+            <div class="row">
+                <div class="col-md-9">
+                    <div id="showEventCalendar"></div>
                 </div>
-                <div data-role="progress" data-value="35" data-cls-bar="bg-red"></div>
-            </div>
-            <div class="mt-6">
-                <div class="clear">
-                    <div class="place-left">Visit Premium Page</div>
-                    <div class="place-right"><strong>480</strong>/800</div>
+                <div class="col-md-3">
+                    <h4>All Events List</h4>
+                    <ul id="eventlist" class="nav nav-list"></ul>
                 </div>
-                <div data-role="progress" data-value="35"></div>
+            </div>	
+            <div style="margin:50px 0px 0px 0px;">
+                <a class="btn btn-default read-more" style="background:#3399ff;color:white" href="http://www.phpzag.com/create-event-calendar-with-jquery-php-and-mysql/">Back to Tutorial</a>		
             </div>
-            <div class="mt-6">
-                <div class="clear">
-                    <div class="place-left">Send Inquiries</div>
-                    <div class="place-right"><strong>250</strong>/500</div>
-                </div>
-                <div data-role="progress" data-value="35" data-cls-bar="bg-orange"></div>
             </div>
-            <div class="mt-6">
-                <p class="text-small">Cum brodium resistere, omnes spatiies perdere varius, magnum lanistaes.</p>
-            </div>
-        </div>
     </div>
 
     <hr>
 </div>
 </div>
-
-<script src="Pandora/source/js/charts.js"></script>
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/underscore-min.js"></script>
+<script type="text/javascript" src="js/calendar.js"></script>
+<script type="text/javascript" src="js/events.js"></script>
