6eb66c4495260aa8878b95fda59f866470f430be
Antonio Guedes <agsoares@gmail.com>
2020-09-19 23:41:28 -0300

    tentando novamente colocar o calendario

--- dashboard.php
+++ dashboard.php
@@ -20,6 +20,32 @@
     <div class="row">
         <div class="cell-md-8 p-10">
         <h5 class="text-center" >Event Calendar with jQuery, PHP and MySQL</h5>	
+        <div class="page-header">
+                <div class="pull-right form-inline">
+                    <div class="btn-group">
+                        <button class="btn btn-primary" data-calendar-nav="prev"><< Prev</button>
+                        <button class="btn btn-default" data-calendar-nav="today">Today</button>
+                        <button class="btn btn-primary" data-calendar-nav="next">Next >></button>
+                    </div>
+                    <div class="btn-group">
+                        <button class="btn btn-warning" data-calendar-view="year">Year</button>
+                        <button class="btn btn-warning active" data-calendar-view="month">Month</button>
+                        <button class="btn btn-warning" data-calendar-view="week">Week</button>
+                        <button class="btn btn-warning" data-calendar-view="day">Day</button>
+                    </div>
+                </div>
+                <h3></h3>
+                <small>To see example with events navigate to Februray 2018</small>
+            </div>
+            <div class="row">
+                <div class="col-md-9">
+                    <div id="showEventCalendar"></div>
+                </div>
+                <div class="col-md-3">
+                    <h4>All Events List</h4>
+                    <ul id="eventlist" class="nav nav-list"></ul>
+                </div>
+            </div>	
             <!--<div class="container">	
             <h2>Event Calendar with jQuery, PHP and MySQL</h2>	
             <div class="page-header">
