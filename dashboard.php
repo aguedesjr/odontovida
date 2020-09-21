5902ced3f3c5436cf592d46d94beca805995b354
Antonio Guedes <agsoares@gmail.com>
2020-09-21 17:14:52 -0300

    alterado as aspas

--- dashboard.php
+++ dashboard.php
@@ -5,83 +5,83 @@
 <script src="lib/main.js"></script>
 <script>
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
+  document.addEventListener("DOMContentLoaded", function() {
+    var calendarEl = document.getElementById("calendar");
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
 
       headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'listDay,listWeek'
+        left: "prev,next today",
+        center: "title",
+        right: "listDay,listWeek"
       },
 
       // customize the button names,
-      // otherwise they'd all just say "list"
+      // otherwise they"d all just say "list"
       views: {
-        listDay: { buttonText: 'list day' },
-        listWeek: { buttonText: 'list week' }
+        listDay: { buttonText: "list day" },
+        listWeek: { buttonText: "list week" }
       },
 
-      initialView: 'listWeek',
-      initialDate: '2020-09-12',
+      initialView: "listWeek",
+      initialDate: "2020-09-12",
       navLinks: true, // can click day/week names to navigate views
       editable: true,
       dayMaxEvents: true, // allow "more" link when too many events
       events: [
         {
-          title: 'All Day Event',
-          start: '2020-09-01'
+          title: "All Day Event",
+          start: "2020-09-01"
         },
         {
-          title: 'Long Event',
-          start: '2020-09-07',
-          end: '2020-09-10'
-        },
-        {
-          groupId: 999,
-          title: 'Repeating Event',
-          start: '2020-09-09T16:00:00'
+          title: "Long Event",
+          start: "2020-09-07",
+          end: "2020-09-10"
         },
         {
           groupId: 999,
-          title: 'Repeating Event',
-          start: '2020-09-16T16:00:00'
+          title: "Repeating Event",
+          start: "2020-09-09T16:00:00"
         },
         {
-          title: 'Conference',
-          start: '2020-09-11',
-          end: '2020-09-13'
+          groupId: 999,
+          title: "Repeating Event",
+          start: "2020-09-16T16:00:00"
         },
         {
-          title: 'Meeting',
-          start: '2020-09-12T10:30:00',
-          end: '2020-09-12T12:30:00'
+          title: "Conference",
+          start: "2020-09-11",
+          end: "2020-09-13"
         },
         {
-          title: 'Lunch',
-          start: '2020-09-12T12:00:00'
+          title: "Meeting",
+          start: "2020-09-12T10:30:00",
+          end: "2020-09-12T12:30:00"
         },
         {
-          title: 'Meeting',
-          start: '2020-09-12T14:30:00'
+          title: "Lunch",
+          start: "2020-09-12T12:00:00"
         },
         {
-          title: 'Happy Hour',
-          start: '2020-09-12T17:30:00'
+          title: "Meeting",
+          start: "2020-09-12T14:30:00"
         },
         {
-          title: 'Dinner',
-          start: '2020-09-12T20:00:00'
+          title: "Happy Hour",
+          start: "2020-09-12T17:30:00"
         },
         {
-          title: 'Birthday Party',
-          start: '2020-09-13T07:00:00'
+          title: "Dinner",
+          start: "2020-09-12T20:00:00"
         },
         {
-          title: 'Click for Google',
-          url: 'http://google.com/',
-          start: '2020-09-28'
+          title: "Birthday Party",
+          start: "2020-09-13T07:00:00"
+        },
+        {
+          title: "Click for Google",
+          url: "http://google.com/",
+          start: "2020-09-28"
         }
       ]
     });
@@ -112,10 +112,10 @@
     </div>
 </div>
 
-<div id='calendar'></div>
+<div id="calendar"></div>
 
 <div class="m-3">
-<div data-role="panel" data-title-caption="Agenda" data-collapsible="false" data-title-icon="<span class='mif-calendar'></span>" class="mt-4">
+<div data-role="panel" data-title-caption="Agenda" data-collapsible="false" data-title-icon="<span class="mif-calendar"></span>" class="mt-4">
     <div class="row">
         <div class="cell-md-8 p-10">
 
