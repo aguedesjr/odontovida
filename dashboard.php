30e871bc24a3268d8761edec1ed8e183ae666938
Antonio Guedes <agsoares@gmail.com>
2020-09-21 17:16:09 -0300

    mais um teste

--- dashboard.php
+++ dashboard.php
@@ -115,11 +115,11 @@
 <div id="calendar"></div>
 
 <div class="m-3">
-<div data-role="panel" data-title-caption="Agenda" data-collapsible="false" data-title-icon="<span class="mif-calendar"></span>" class="mt-4">
+<div data-role="panel" data-title-caption="Agenda" data-collapsible="false" data-title-icon='<span class="mif-calendar"></span>' class="mt-4">
     <div class="row">
         <div class="cell-md-8 p-10">
 
-            
+        <div id="calendar"></div>
         </div>
     </div>
 
