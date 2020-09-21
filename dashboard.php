4c74400c8efeed6098599cde89ab7b6b78dc2892
Antonio Guedes <agsoares@gmail.com>
2020-09-21 17:36:55 -0300

    tentando colocar versão simples do calendario

--- dashboard.php
+++ dashboard.php
@@ -5,99 +5,15 @@
 <script src="lib/main.js"></script>
 <script>
 
-  document.addEventListener("DOMContentLoaded", function() {
-    var calendarEl = document.getElementById("calendar");
-
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-
-      headerToolbar: {
-        left: "prev,next today",
-        center: "title",
-        right: "listDay,listWeek"
-      },
-
-      // customize the button names,
-      // otherwise they"d all just say "list"
-      views: {
-        listDay: { buttonText: "list day" },
-        listWeek: { buttonText: "list week" }
-      },
-
-      initialView: "listWeek",
-      initialDate: "2020-09-12",
-      navLinks: true, // can click day/week names to navigate views
-      editable: true,
-      dayMaxEvents: true, // allow "more" link when too many events
-      events: [
-        {
-          title: "All Day Event",
-          start: "2020-09-01"
-        },
-        {
-          title: "Long Event",
-          start: "2020-09-07",
-          end: "2020-09-10"
-        },
-        {
-          groupId: 999,
-          title: "Repeating Event",
-          start: "2020-09-09T16:00:00"
-        },
-        {
-          groupId: 999,
-          title: "Repeating Event",
-          start: "2020-09-16T16:00:00"
-        },
-        {
-          title: "Conference",
-          start: "2020-09-11",
-          end: "2020-09-13"
-        },
-        {
-          title: "Meeting",
-          start: "2020-09-12T10:30:00",
-          end: "2020-09-12T12:30:00"
-        },
-        {
-          title: "Lunch",
-          start: "2020-09-12T12:00:00"
-        },
-        {
-          title: "Meeting",
-          start: "2020-09-12T14:30:00"
-        },
-        {
-          title: "Happy Hour",
-          start: "2020-09-12T17:30:00"
-        },
-        {
-          title: "Dinner",
-          start: "2020-09-12T20:00:00"
-        },
-        {
-          title: "Birthday Party",
-          start: "2020-09-13T07:00:00"
-        },
-        {
-          title: "Click for Google",
-          url: "http://google.com/",
-          start: "2020-09-28"
-        }
-      ]
+    document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth'
+        });
+        calendar.render();
     });
 
-    calendar.render();
-  });
-
 </script>
-<style>
-
-  body {
-    font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
-    font-size: 14px;
-  }
-
-</style>
 
 <div class="row border-bottom bd-lightGray m-3">
     <div class="cell-md-4 d-flex flex-align-center">
