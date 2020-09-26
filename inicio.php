d8f61c5969f08b477732fecd587e4fb1ffe9ae3e
Antonio Guedes <agsoares@gmail.com>
2020-09-25 21:38:20 -0300

    teste da data

--- inicio.php
+++ inicio.php
@@ -11,16 +11,8 @@
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
         <link href='lib/main.css' rel='stylesheet' />
-        <script src='lib/main.js'></script>
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="Bootstrap/dist/js/scripts.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="Bootstrap/dist/assets/demo/chart-area-demo.js"></script>
-        <script src="Bootstrap/dist/assets/demo/chart-bar-demo.js"></script>
-        <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-        <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-        <script src="Bootstrap/dist/assets/demo/datatables-demo.js"></script>
+        <script src='lib/main.js'></script>
         <script>
 
   document.addEventListener('DOMContentLoaded', function() {
@@ -57,7 +49,7 @@
  $(document).ready(function() {
             $("button").click(function(){
             var today = new Date();
-            var teste = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+            var teste = today.getFullYear()+'-'+(today.getMonth()+1 < 10) ? '0'+(today.getMonth()+1) : (today.getMonth()+1)+'-'+today.getDate();
             alert(teste);
         });
         });
@@ -770,5 +762,14 @@
                 </footer>
             </div>
         </div>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="Bootstrap/dist/js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="Bootstrap/dist/assets/demo/chart-area-demo.js"></script>
+        <script src="Bootstrap/dist/assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+        <script src="Bootstrap/dist/assets/demo/datatables-demo.js"></script>
     </body>
 </html>
