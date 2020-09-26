1337e5b7eab4247367b584be505128602666ed0c
Antonio Guedes <agsoares@gmail.com>
2020-09-25 22:23:41 -0300

    retirado scripts desnecessarios e alterado visualização do calendario

--- inicio.php
+++ inicio.php
@@ -191,7 +191,7 @@ $login = $_SESSION['login'];
                         <ol class="breadcrumb mb-4">
                             <li class="breadcrumb-item active"></li>
                         </ol>
-                        <div class="card mb-5">
+                        <div class="row">
                             <div id='script-warning'>
                                 <code>php/get-events.php</code> must be running.
                             </div>
@@ -217,11 +217,5 @@ $login = $_SESSION['login'];
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="Bootstrap/dist/js/scripts.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="Bootstrap/dist/assets/demo/chart-area-demo.js"></script>
-        <script src="Bootstrap/dist/assets/demo/chart-bar-demo.js"></script>
-        <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-        <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-        <script src="Bootstrap/dist/assets/demo/datatables-demo.js"></script>
     </body>
 </html>
