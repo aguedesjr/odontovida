f7a2af8feef311111db1549f407fc7387679fcb9
Antonio Guedes <agsoares@gmail.com>
2020-09-29 23:19:02 -0300

    ajustado scripts

--- inicio.php
+++ inicio.php
@@ -17,7 +17,10 @@ $login = $_SESSION['login'];
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
         <link href='lib/main.css' rel='stylesheet' />
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="Bootstrap/dist/js/scripts.js"></script>
         <script src='lib/main.js'></script>
         <script>
 
@@ -360,9 +363,5 @@ $(document).ready(function () {
         </div>
         <!-- Modal de Cadastro de Usuários -->
 
-
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="Bootstrap/dist/js/scripts.js"></script>
     </body>
 </html>
