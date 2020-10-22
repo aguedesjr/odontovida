c017fd0fd4ac8a207b5def5321ec500e22fe4a7e
Antonio Guedes <agsoares@gmail.com>
2020-10-22 00:03:06 -0300

    teste de alteração no bd

--- configs/managebd.php
+++ configs/managebd.php
@@ -68,10 +68,10 @@ if ($comando == "alterarEvento") {
     $data_end = str_replace('/', '-', $_POST['newEnd']);
     $data_end_conv = date("Y-m-d H:i:s", strtotime($data_end));
     echo $data_end_conv;
-    /*$sql = "UPDATE events SET
+    $sql = "UPDATE events SET
     (start,end) 
     VALUES 
-    ('$data_start_conv','$data_end_conv') WHERE id = $id;";*/
+    ('$data_start_conv','$data_end_conv') WHERE id = '$id';";
 
     //if ($conn->query($sql) === TRUE) {
         //$_SESSION['agendastatus']="sucesso";
