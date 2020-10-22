6e90db9a202605b546b3fffba3544c6d92d662f1
Antonio Guedes <agsoares@gmail.com>
2020-10-21 21:42:10 -0300

    tirado o id

--- inicio.php
+++ inicio.php
@@ -89,7 +89,7 @@ $login = $_SESSION['login'];
                     type: "POST",
                     url: "configs/managebd.php",
                     data: {
-                        id: info.event.id,
+                        //id: info.event.id,
                         newStart: info.event.start.toLocaleString(),
                         newEnd: info.event.end.toLocaleString(),
                         comando: "alterarEvento"
