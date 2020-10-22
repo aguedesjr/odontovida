d6b407e8403914891f6f64e1ae7c17a97ab5acc5
Antonio Guedes <agsoares@gmail.com>
2020-10-21 21:43:30 -0300

    adicionado o function

--- inicio.php
+++ inicio.php
@@ -89,13 +89,13 @@ $login = $_SESSION['login'];
                     type: "POST",
                     url: "configs/managebd.php",
                     data: {
-                        //id: info.event.id,
+                        id: info.event.id,
                         newStart: info.event.start.toLocaleString(),
                         newEnd: info.event.end.toLocaleString(),
                         comando: "alterarEvento"
                     },
-                    success: {
-                        console.log("Teste");
+                    success: function() {
+                        console.log(data);
                     }
                         /*function(data) {
                         Swal.fire({
