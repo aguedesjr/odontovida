10cdd0504f0b504b21b70618422cabeb3d759d84
Antonio Guedes <agsoares@gmail.com>
2020-10-22 22:47:47 -0300

    mais um ajuste

--- inicio.php
+++ inicio.php
@@ -296,12 +296,12 @@ $login = $_SESSION['login'];
                                 Dashboard
                             </a>
                             <div class="sb-sidenav-menu-heading">Consultório</div>
-                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pacientes" aria-expanded="false" aria-controls="collapseLayouts">
                                 <div class="sb-nav-link-icon"><i class="far fa-address-card"></i></div>
                                 Pacientes
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                            <div class="collapse" id="pacientes" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
                                     <a class="nav-link" href="layout-static.html">Cadastrar</a>
                                     <a class="nav-link" href="layout-sidenav-light.html">Listar</a>
@@ -339,12 +339,12 @@ $login = $_SESSION['login'];
                                 </nav>
                             </div>
                             <div class="sb-sidenav-menu-heading">Sistema</div>
-                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#usuarios" aria-expanded="false" aria-controls="collapseLayouts">
                                 <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
                                 Usuários
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                            <div class="collapse" id="usuarios" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
                                     <a class="nav-link" href="layout-static.html">Cadastrar</a>
                                     <a class="nav-link" href="layout-sidenav-light.html">Listar</a>
