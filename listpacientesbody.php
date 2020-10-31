9ee45d6996616305f2fbb19f13750a9dae01b48b
Antonio Guedes <agsoares@gmail.com>
2020-10-30 21:27:21 -0300

    enviando scripts do datatable

--- listpacientesbody.php
+++ listpacientesbody.php
@@ -1,20 +1,12 @@
 <div class="container-fluid">
-                        <h1 class="mt-4">Tables</h1>
+                        <h1 class="mt-4">Pacientes</h1>
                         <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                            <li class="breadcrumb-item active">Tables</li>
+                            <li class="breadcrumb-item active"></li>
                         </ol>
-                        <div class="card mb-4">
-                            <div class="card-body">
-                                DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the
-                                <a target="_blank" href="https://datatables.net/">official DataTables documentation</a>
-                                .
-                            </div>
-                        </div>
                         <div class="card mb-4">
                             <div class="card-header">
                                 <i class="fas fa-table mr-1"></i>
-                                DataTable Example
+                                Listar
                             </div>
                             <div class="card-body">
                                 <div class="table-responsive">
@@ -502,3 +494,6 @@
                             </div>
                         </div>
                     </div>
+                    <script src="js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+                    <script src="js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+                    <script src="js/datatables.js"></script>
