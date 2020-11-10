5725d58addf901a99e73a3c4b50f77d0d4b40914
Antonio Guedes <agsoares@gmail.com>
2020-11-10 18:26:01 -0300

    alterado botão da listagem de pacientes

--- listpacientesbody.php
+++ listpacientesbody.php
@@ -28,8 +28,8 @@
                                             <tr>
                                                 <th><? echo $row[0]; ?></th>
                                                 <th><? echo $row[1]; ?></th>
-                                                <th style="align: center;"><button type="submit" class="btn btn-primary"><i class="fas fa-user-edit"></i> Editar</button></th>
-                                                <th style="align: center;"><button type="submit" class="btn btn-primary"><i class="fas fa-user-minus"></i> Apagar</button></th>
+                                                <th style="align: center;"><button type="submit" class="btn btn-warning"><i class="fas fa-user-edit"></i> Editar</button></th>
+                                                <th style="align: center;"><button type="submit" class="btn btn-danger"><i class="fas fa-user-minus"></i> Apagar</button></th>
                                             </tr>
                                             <?};
                                              /* free result set */
