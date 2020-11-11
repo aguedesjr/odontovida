ad286d9c2a5f93e05cd94cc678d8095418416642
Antonio Guedes <agsoares@gmail.com>
2020-11-10 23:38:42 -0300

    adicionado o jquery-ui para o autocomplete funcionar

--- inicio.php
+++ inicio.php
@@ -24,6 +24,7 @@ $login = $_SESSION['login'];
         <script src="js/all.min.js" crossorigin="anonymous"></script>
         <link href='lib/main.css' rel='stylesheet' />
         <script src="js/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+        <script src="js/jquery-ui.js" crossorigin="anonymous"></script>
         <script src="js/popper.min.js"></script>
         <script src="js/sweetalert2@10.js"></script>
         <script src="js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
