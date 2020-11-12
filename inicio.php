73b0280bde346447bf0242142ffcd5691cafcb4e
Antonio Guedes <agsoares@gmail.com>
2020-11-12 01:17:44 -0300

    tentando colocar o autocomplete no modal

--- inicio.php
+++ inicio.php
@@ -45,7 +45,7 @@ $login = $_SESSION['login'];
                     });
                     
                     // Chamo o Auto complete do JQuery ui setando o id do input, array com os dados e o mínimo de caracteres para disparar o AutoComplete
-                    $('#cadastrar #title').autocomplete({ source: paciente, minLength: 3});
+                    $('#cadastrar #title').autocomplete({ modal: true, source: paciente, minLength: 3});
                 });
             });
         </script>
