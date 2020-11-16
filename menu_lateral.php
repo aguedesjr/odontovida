4bfd3d30951321f2f1322d85a514084aacf68618
Antonio Guedes <agsoares@gmail.com>
2020-11-16 00:24:02 -0300

    adicionado o menu convenio

--- menu_lateral.php
+++ menu_lateral.php
@@ -9,8 +9,19 @@
                             </a>
                             <div class="sb-sidenav-menu-heading">Consultório</div>
                             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pacientes" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="sb-nav-link-icon"><i class="far fa-address-card"></i></div>
+                                <div class="sb-nav-link-icon"><i class="fas fa-hospital-user"></i></div>
                                 Pacientes
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="pacientes" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="cadpacientes.php">Cadastrar</a>
+                                    <a class="nav-link" href="listpacientes.php">Listar</a>
+                                </nav>
+                            </div>
+                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pacientes" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"><i class="far fa-id-card"></i></div>
+                                Convênios
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
                             <div class="collapse" id="pacientes" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
--- menu_topo_user.php
+++ menu_topo_user.php
@@ -2,10 +2,10 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                        <a class="dropdown-item" href="#">Settings</a>
+                        <!--<a class="dropdown-item" href="#">Settings</a>
                         <a class="dropdown-item" href="#">Activity Log</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="logout.php">Logout</a>
+                        <div class="dropdown-divider"></div>-->
+                        <a class="dropdown-item" href="logout.php">Sair</a>
                     </div>
                 </li>
             </ul>
