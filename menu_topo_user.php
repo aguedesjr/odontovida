8e9f4b51f4063a6947ebcc1cf653b728141cd670
Antonio Guedes <agsoares@gmail.com>
2020-11-18 19:46:36 -0300

    ajustado menu superior

--- menu_topo_user.php
+++ menu_topo_user.php
@@ -1,11 +1,11 @@
 <ul class="navbar-nav ml-auto ml-md-0">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                        <!--<a class="dropdown-item" href="#">Settings</a>
-                        <a class="dropdown-item" href="#">Activity Log</a>
-                        <div class="dropdown-divider"></div>-->
-                        <a class="dropdown-item" href="logout.php"><i class="fas fa-sign-out-alt"></i>Sair</a>
-                    </div>
-                </li>
-            </ul>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+            <!--<a class="dropdown-item" href="#">Settings</a>
+            <a class="dropdown-item" href="#">Activity Log</a>
+            <div class="dropdown-divider"></div>-->
+            <a class="dropdown-item" href="logout.php"><i class="fas fa-sign-out-alt" style="color:red"></i> Sair</a>
+        </div>
+    </li>
+</ul>
