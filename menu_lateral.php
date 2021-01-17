43e12c4351acb4919bf08f45b58ca7b3873f8fd8
“Antonio <agsoares@gmail.com>
2021-01-17 19:32:06 -0300

    Adicionado menu de procedimentos

--- menu_lateral.php
+++ menu_lateral.php
@@ -30,6 +30,17 @@
                                     <a class="nav-link" href="listconvenio.php">Listar</a>
                                 </nav>
                             </div>
+                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#procedimentos" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"><i class="far fa-id-card" style="color:yellow"></i></div>
+                                Procedimentos
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="procedimentos" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="cadprocedimentos.php">Cadastrar</a>
+                                    <a class="nav-link" href="listprocedimentos.php">Listar</a>
+                                </nav>
+                            </div>
                             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
                                 <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
                                 Pages
